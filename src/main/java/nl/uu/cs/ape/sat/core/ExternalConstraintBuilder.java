fcc4fdc8c365f2f6a277d9d0cdbf6359b2b2e8d3
Stan <stanvanmeerendonk@gmail.com>
2020-03-17 14:37:33 +0100

    Add external Constraint builder interface

---
+++ src/main/java/nl/uu/cs/ape/sat/core/ExternalConstraintBuilder.java
@@ -0,0 +1,16 @@
+package nl.uu.cs.ape.sat.core;
+
+import nl.uu.cs.ape.sat.automaton.ModuleAutomaton;
+import nl.uu.cs.ape.sat.automaton.TypeAutomaton;
+import nl.uu.cs.ape.sat.models.AtomMappings;
+import nl.uu.cs.ape.sat.utils.APEDomainSetup;
+
+public interface ExternalConstraintBuilder {
+
+	public StringBuilder Build(
+		APEDomainSetup domainSetup,
+		ModuleAutomaton moduleAutomaton,
+		TypeAutomaton typeAutomaton,
+		AtomMappings atomDictionary
+	);
+}
