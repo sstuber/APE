73873143bfdb82f0f5475ec858212b5ff614810a
Stan <stanvanmeerendonk@gmail.com>
2020-03-30 15:08:55 +0200

    Use External in example This way it does not disturb the original representation

--- src/main/java/nl/uu/cs/ape/sat/core/extrernal/ExternalConstraintExample.java
+++ src/main/java/nl/uu/cs/ape/sat/core/extrernal/ExternalConstraintExample.java
@@ -6,6 +6,7 @@ import nl.uu.cs.ape.sat.automaton.TypeAutomaton;
 import nl.uu.cs.ape.sat.models.AbstractModule;
 import nl.uu.cs.ape.sat.models.AtomMappings;
 import nl.uu.cs.ape.sat.models.Module;
+import nl.uu.cs.ape.sat.models.enums.NodeType;
 import nl.uu.cs.ape.sat.models.enums.WorkflowElement;
 import nl.uu.cs.ape.sat.models.logic.constructs.Atom;
 import nl.uu.cs.ape.sat.utils.APEDomainSetup;
@@ -17,16 +18,23 @@ public class ExternalConstraintExample implements ExternalConstraintBuilder {
 
 		AbstractModule module = domainSetup.getAllModules().get("add_cpt");
 
+		Module externalModule = new Module("test", "test", "ToolsTaxonomy", null);
+
+		//AbstractModule externalModule = new AbstractModule("test", "test", "ToolsTaxonomy", NodeType.LEAF);
+
 		State state = moduleAutomaton.getModuleStates().get(0);
 
 		//Atom atom = new Atom(module, state, WorkflowElement.MODULE );
 
 		Integer id = atomDictionary.add(module, state, WorkflowElement.MODULE);
+		Integer exId = atomDictionary.add(externalModule, state, WorkflowElement.EXTERNAL);
 
 		StringBuilder result = new StringBuilder()
 			.append(id)
+			.append(" 0\n")
+			.append(exId)
 			.append(" 0\n");
-		
+
 		return result;
 	}
 }
